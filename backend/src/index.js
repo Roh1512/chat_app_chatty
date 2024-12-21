f4122a0beeadb11afcbeb1b086c17c7e946b0b5e
Rohith19 <rohith1512a@gmail.com>
2024-12-21 21:19:14 +0530

    messages bug fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -28,7 +28,7 @@ app.use(
   })
 );
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "development") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
     return res.sendFile(
